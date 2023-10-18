568c8a6957532831404dbfd41a8b5d1a9a6a50e1
AmmarNaeemJanjua <anjanjua99@gmail.com>
2023-10-18 09:31:15 +0500

    update

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -15,7 +15,7 @@
                         <v-card-actions class="d-flex justify-center">
 
                             <v-btn color="deep-purple-darken-3" @click="viewPost(post)">
-                                VIEW FULL PO
+                                VIEW FULL POST
                             </v-btn>
 
                         </v-card-actions>
